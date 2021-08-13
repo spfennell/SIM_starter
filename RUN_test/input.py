f3c781e8d786290f1d2974bf6153db8a7130ad1a
Scott Fennell <spfennell@gmail.com>
2021-08-13 10:15:11 -0500

    monte carlo input file

--- RUN_test/input.py
+++ RUN_test/input.py
@@ -1,5 +1,7 @@
 
-
+trick.mc_set_enabled(1)
+trick.mc_set_num_runs(5)
+trick.mc_add_slave("localhost")
 trick.stop(15.0);
-#trick.real_time_enable()
-#trick.sim_control_panel_set_enabled(True)
+trick.real_time_enable()
+trick.sim_control_panel_set_enabled(True)
